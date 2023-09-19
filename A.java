0ff225627d202aa01b48a560d2b0220fe6402c88
aroon-pk <aroonpk11@gmail.com>
2023-09-19 16:59:42 +0530

    first commit

--- A.java
+++ A.java
@@ -3,5 +3,9 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("i am Arun");
+		System.out.println("i am Arun");
+		System.out.println("i am Arun");
+		System.out.println("i am Arun");
+		System.out.println("i am Arun");
 	}
 }
